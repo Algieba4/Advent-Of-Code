b2ef437ff585ba0e8c5d3107d06495da4963b17e
Travis Ottelien <>
2021-12-05 23:12:04 -0600

    Updated code for day-1 part-2

--- 2021/day1.py
+++ 2021/day1.py
@@ -8,20 +8,35 @@ Author: Travis Ottelien
 
 if __name__ == "__main__":
 
+	arrayCount = 0
 	count = 0
+	depths = []
 	firstLine = "True"
 	inputFile = "C:\\dev\\GitHub\\Advent-Of-Code\\2021\\day1.txt"
 		
 	with open(inputFile) as fp:
+	
 		for line in fp:
-			if(bool(firstLine)):
+			
+			if len(depths) > int(0):
 				currentLine = line
-				firstLine = ""
+				if int(currentLine) > int(previousLine):
+					count = count + 1
 			else:
 				currentLine = line
-				if(int(currentLine) > int(previousLine)):
-					count = count + 1
+		
+			if len(depths) < int(3):
+				depths.append(line)
+			else:
+				previousArrayCount = int(depths[0]) + int(depths[1]) + int(depths[2])
+				depths.pop(0)
+				depths.append(line)
+				currentArrayCount = int(depths[0]) + int(depths[1]) + int(depths[2])
+				if int(currentArrayCount) > int(previousArrayCount):
+					arrayCount = arrayCount + 1
+
 			previousLine = currentLine
 	
 	print('There are ' + str(count) + ' measurements that are larger than the previous measurement.')
+	print('There are ' + str(arrayCount) + ' sums that are larger than the previous sum')
 						
