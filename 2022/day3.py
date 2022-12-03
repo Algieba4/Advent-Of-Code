fbbeeaaf57aa9fd7f7c30ed6748bc0ce29d15512
Travis Ottelien <>
2022-12-03 13:31:22 -0600

    Day 3; Part 3

--- 2022/day3.py
+++ 2022/day3.py
@@ -15,18 +15,40 @@ PmmdzqPrVvPwwTWBwg
 wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
 ttgJtRGJQctTZtZT
 CrZsJsPPZsGzwwsLwLmpwMDw
-
 '''	
 
-def get_common_item(compartment_one, compartment_two):
+def get_badge_common_item(rucksack_group):
+	rucksack_one = rucksack_group.pop()
+	rucksack_two = rucksack_group.pop()
+	rucksack_thre = rucksack_group.pop()
+	
+	for item in rucksack_one:
+		if item in rucksack_two:
+			if item in rucksack_thre:
+				return item
+
+def get_badge_priority(rucksack_group):
+
+	item = get_badge_common_item(rucksack_group)
+	priority = 0
+	
+	if(item.isupper()):
+		priority = ((ord(item) - 64) + 26)
+	else:
+		priority = ord(item) - 96
+	
+	print("Common Group Item: " + str(item) + " ; Priority: " + str(priority))
+	print()
+	return priority
+
+def get_rucksack_common_item(compartment_one, compartment_two):
 	for item in compartment_one:
 		if item in compartment_two:
 			return item
-	
 
-def get_priority(compartment_one, compartment_two):
+def get_rucksack_priority(compartment_one, compartment_two):
 
-	item = get_common_item(compartment_one, compartment_two)
+	item = get_rucksack_common_item(compartment_one, compartment_two)
 	priority = 0
 	
 	if(item.isupper()):
@@ -40,10 +62,14 @@ def get_priority(compartment_one, compartment_two):
 if __name__ == "__main__":
 	
 	inputFile = "C:\\dev\\GitHub\\Advent-Of-Code\\2022\\day3.txt"
+	sum_group_priority = 0
 	sum_priority = 0
 	
 	with open(inputFile) as fp:
+		count = 0
+		rucksack_group = []
 		for line in fp:
+			count += 1
 			rucksack = list(line.strip())
 			rucksack_size = len(rucksack)
 			rucksack_first_half = rucksack_size / 2
@@ -59,8 +85,16 @@ if __name__ == "__main__":
 				compartment_two.append(rucksack[i])
 				i = i + 1
 			
-			priority = get_priority(compartment_one, compartment_two)
+			priority = get_rucksack_priority(compartment_one, compartment_two)
 			sum_priority += int(priority)
+			
+			rucksack_group.append(rucksack)
+			if(count == 3):
+				group_priority = get_badge_priority(rucksack_group)
+				sum_group_priority += int(group_priority)
+				count = 0
+				rucksack_group = []
 	
-	print("Sum of the priorities: " + str(sum_priority))
+	print("Sum of the rucksack priorities: " + str(sum_priority))
+	print("Sum of the group priorities: " + str(sum_group_priority))
 			
