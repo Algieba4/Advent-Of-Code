2c569b9e91d20678bcbe18ac339b964e2a28925a
Travis Ottelien <>
2022-12-08 22:30:15 -0600

    Day 7 ; Part 2 ; Failed

--- 2022/day7.py
+++ 2022/day7.py
@@ -31,6 +31,8 @@ $ ls
 8033020 d.log
 5626152 d.ext
 7214296 k
+
+It's lower than 17,545,234
 '''
 
 
@@ -44,33 +46,52 @@ if __name__ == "__main__":
 	with open(inputFile) as fp:
 		for line in fp:
 			line_info = line.split()
+			
+			# Change directory will manipulate the path array
 			if 'cd' in line_info:
 				if '..' in line_info:
 					current_path.pop()
 				else:
-					current_path.append(line_info[2])
+					list_path = list(line_info[2])
+					for item in list_path:
+						current_path.append(item)
+						
+			# These lines don't do anything
 			elif 'ls' in line_info or 'dir' in line_info:
 				continue
+				
+			# Whatever is left are the files
 			else:
-				path = ''.join(current_path)
-				bytes = line_info[0]
-				temp_path = current_path.copy()
-				i = len(temp_path)
+				bytes = line_info[0] # The size of the file
+				path = current_path.copy() # Creates a temp path array without corrupting the original
+				i = len(path) # Used to go backward down the file path (e.g. /egg --> /eg --> /e --> /)
 				while i >= 1:
-					rolling_path = ''.join(temp_path)
-					if rolling_path in directories:
-						directories[rolling_path] = int(bytes) + int(directories[rolling_path])
+					path_index = ''.join(path) # Converts the temp path array into a string (index)
+					if path_index in directories:
+						directories[path_index] = int(bytes) + int(directories[path_index])
 					else:
-						directories[rolling_path] = bytes
-					temp_path.pop()
-					i = len(temp_path)
-	print(directories)
+						directories[path_index] = bytes
+					path.pop()
+					i = len(path)
 	
 	answer = 0
 	for key in directories:
 		if int(directories[key]) < 100000:
 			answer += int(directories[key])
-	print("Answer: " + str(answer))
+	print("Total filesize in which a directory is less than 100,000 bytes : " + str(answer))
+	
+	total_used_disk = directories["/"]
+	print("Total used disk: " + str(total_used_disk))
+	target = 30000000
+	answer = directories["/"]
+	for key in directories:
+		difference = int(total_used_disk) - int(directories[key])
+		if int(difference) < target:
+			temp_answer = directories[key]
+			if int(temp_answer) < int(answer):
+				answer = temp_answer
+	
+	print("Filesize required for deletion: " + str(answer))
 
 
 
