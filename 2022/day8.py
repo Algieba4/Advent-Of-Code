0b642e63698ffe192de59279dd39a97c99531e7b
Travis Ottelien <>
2022-12-08 23:43:32 -0600

    Day 8; Part 2

--- 2022/day8.py
+++ 2022/day8.py
@@ -10,7 +10,6 @@ TEST DATA
 65332
 33549
 35390
-
 '''
 
 def count_visible_trees(tree_grove):
@@ -30,29 +29,89 @@ def count_visible_trees(tree_grove):
 	while row < rows - 1:
 		while collumn < collumns -1:
 			if is_visible_down(tree_grove, row, collumn, range(int(row) + 1, rows)):
-				# print("row " + str(row) + " collumn " + str(collumn) + " is visible from below")
 				count += 1
 			elif is_visible_left(tree_grove, row, collumn, range(0, collumn)):
-				# print("row " + str(row) + " collumn " + str(collumn) + " is visible from left")
 				count += 1
 			elif is_visible_right(tree_grove, row, collumn, range(int(collumn) + 1, collumns)):
-				# print("row " + str(row) + " collumn " + str(collumn) + " is visible from right")
 				count += 1
 			elif is_visible_up(tree_grove, row, collumn, range(0, row)):
-				# print("row " + str(row) + " collumn " + str(collumn) + " is visible from up")
 				count += 1
 			collumn += 1
 		row += 1
 		collumn = 1
 		
 	return count
+	
+def count_scenic_trees(tree_grove):
+	row = 1
+	collumn = 1
+	rows = len(tree_grove)
+	collumns = len(tree_grove[0])
+	
+	highest_scenic_score = 0
+	while row < rows - 1:
+		while collumn < collumns - 1:
+			down_scenic_score = get_down_scenic_score(tree_grove, row, collumn, range(int(row) + 1, rows))
+			left_scenic_score = get_left_scenic_score(tree_grove, row, collumn, range(0, collumn))
+			right_scenic_score = get_right_scenic_score(tree_grove, row, collumn, range(int(collumn) + 1, collumns))
+			up_scenic_score = get_up_scenic_score(tree_grove, row, collumn, range(0, row))
+			total_scenic_score = down_scenic_score * left_scenic_score * right_scenic_score * up_scenic_score
+			if(total_scenic_score > highest_scenic_score):
+				highest_scenic_score = int(total_scenic_score)
+			collumn += 1
+		row += 1
+		collumn = 1
 
-def print_tree_grove(tree_grove):
-	for tree_row in tree_grove:
-		for tree in tree_row:
-			print(tree,end = " ")
-		print()
-		
+	return highest_scenic_score
+
+def get_down_scenic_score(tree_grove, row, collumn, range):
+	scenic_score = 0
+	
+	for number in range:
+		if tree_grove[row][collumn] > tree_grove[number][collumn]:
+			scenic_score += 1
+		if tree_grove[row][collumn] <= tree_grove[number][collumn]:
+			scenic_score += 1
+			return scenic_score
+	
+	return scenic_score
+
+def get_left_scenic_score(tree_grove, row, collumn, range):
+	scenic_score = 0
+	
+	for number in reversed(range):
+		if tree_grove[row][collumn] > tree_grove[row][number]:
+			scenic_score += 1
+		if tree_grove[row][collumn] <= tree_grove[row][number]:
+			scenic_score += 1
+			return scenic_score
+	
+	return scenic_score
+
+def get_right_scenic_score(tree_grove, row, collumn, range):
+	scenic_score = 0
+	
+	for number in range:
+		if tree_grove[row][collumn] > tree_grove[row][number]:
+			scenic_score += 1
+		if tree_grove[row][collumn] <= tree_grove[row][number]:
+			scenic_score += 1
+			return scenic_score
+	
+	return scenic_score
+
+def get_up_scenic_score(tree_grove, row, collumn, range):
+	scenic_score = 0
+	
+	for number in reversed(range):
+		if tree_grove[row][collumn] > tree_grove[number][collumn]:
+			scenic_score += 1
+		if tree_grove[row][collumn] <= tree_grove[number][collumn]:
+			scenic_score += 1
+			return scenic_score
+	
+	return scenic_score
+
 def is_visible_down(tree_grove, row, collumn, range):
 	for number in range:
 		if tree_grove[row][collumn] <= tree_grove[number][collumn]:
@@ -77,6 +136,12 @@ def is_visible_up(tree_grove, row, collumn, range):
 			return False
 	return True
 
+def print_tree_grove(tree_grove):
+	for tree_row in tree_grove:
+		for tree in tree_row:
+			print(tree,end = " ")
+		print()
+
 if __name__ == "__main__":
 	
 	inputFile = "day8.txt"
@@ -88,7 +153,9 @@ if __name__ == "__main__":
 			tree_grove.insert(index, list(line.strip()))
 			index += 1
 		
-	print_tree_grove(tree_grove)
-	count = count_visible_trees(tree_grove)
+	# print_tree_grove(tree_grove)
+	count_visible = count_visible_trees(tree_grove)
+	count_scenic = count_scenic_trees(tree_grove)
 	
-	print("count of visible trees: " + str(count))
+	print("count of visible trees: " + str(count_visible))
+	print("highest scenic score possible: " + str(count_scenic))
