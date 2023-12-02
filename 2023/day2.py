d6916148c63c198528c4a2e880c7a4085c56fbab
Travis O <travisottelien@gmail.com>
2023-12-02 12:02:55 -0600

    day2 ; part2

--- 2023/day2.py
+++ 2023/day2.py
@@ -10,7 +10,6 @@ Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
 Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
-
 """
 
 def possible_ids(_line):    
@@ -64,15 +63,82 @@ def is_over_max(_cubes):
             return True
 
     return False;
+    
+
+def get_game_power(_line):
+    game,rounds = _line.split(":")
+    
+    text,game_id = game.strip().split()
+    
+    rounds_list = rounds.strip().split(";")
+    power = get_round_power(rounds_list)
+    
+    print("round " + str(game_id) + " has a power of " + str(power))
+    return power    
+
+
+def get_round_power(_rounds_list):
+    #[red,green,blue]
+    powers = [0,0,0]
+
+    i = 0
+    rounds_length = len(_rounds_list)
+    while i < rounds_length:
+        current_round = _rounds_list[i]
+        cubes = current_round.strip().split(",")
+                
+        j = 0
+        cubes_length = len(cubes)
+        while j < cubes_length:
+            powers = get_cube_powers(cubes[j], powers)
+            j += 1
+        
+        i += 1
+    
+    return multiple_powers(powers)
+
+
+def get_cube_powers(_cubes, _powers):
+    number,color = _cubes.strip().split()
+        
+    if color.strip() == "red":
+        current_power = _powers[0]
+        if int(number) > int(current_power):
+            _powers[0] = number
+    elif color.strip() == "green":
+        current_power = _powers[1]
+        if int(number) > int(current_power):
+            _powers[1] = number
+    elif color.strip() == "blue":
+        current_power = _powers[2]
+        if int(number) > int(current_power):
+            _powers[2] = number
+
+    return _powers
+
+def multiple_powers(_powers):
+    power = int(_powers[0])
+    
+    i = 1
+    power_length = len(_powers)
+    while i < power_length:
+        power *= int(_powers[i])
+        i += 1
+        
+    return power
+
 
 if __name__ == "__main__":
     id_sum = 0
+    power_sum = 0
     inputFile = "C:\\github\\Advent-Of-Code\\2023\\day2.txt"
     
     with open(inputFile) as fp:
         for line in fp:
             id_sum += int(possible_ids(line))
+            power_sum += int(get_game_power(line))
 
     print()
     print('Total calibration: ' + str(id_sum))
+    print('Total power: ' + str(power_sum))
     
